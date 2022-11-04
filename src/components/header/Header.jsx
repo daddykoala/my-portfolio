89339bdd3152922ecc864168e876d7683da173c0
daddykoala <moretticlement@outlook.fr>
2022-11-04 14:16:39 +0100

    ajout des titres et des calltoaction du header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 //import PropTypes from 'prop-types';
+import CallToAction from './callToAction';
+import imageprofil from '../../assets/imageprofil.png'
 
 import './headerStyles.scss';
 
 function Header() {
 return (
-    <div>
-        header
-    </div>
+    <header>
+        <div className="container container__header">
+            <h5 >hello i'm</h5>
+            <h1>Moretti Clément</h1>
+            <h5 className="text-light">Fullstack developpeur</h5>
+            <CallToAction/>
+            <div className="me">
+                <img src={imageprofil} alt="image d'un développeur fullstack" />
+                <a href="#contact" className='scroll_down'>ScrollDown</a>
+            </div>
+        </div>
+    </header>
 );
 }
 //header.propTypes = {};
