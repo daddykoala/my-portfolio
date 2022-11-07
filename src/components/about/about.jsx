06f97721c63aab8a6d507aab095666fdd1f6cbf2
daddykoala <moretticlement@outlook.fr>
2022-11-07 14:38:12 +0100

    add:structure about component

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,12 +1,46 @@
 import React from 'react';
 //import PropTypes from 'prop-types';
+import CLEMENT from '../../assets/clement.png';
+import { FaAward } from 'react-icons/fa';
 
-import './aboutStyles.scss';
+
+// import './aboutStyles.scss';
 
 function About() {
-return (
-    <section id='about'>about</section>
-);
+    return (
+        <section id='about'>
+            <h5>Get to Know</h5>
+            <h2>About Me</h2>
+
+            <div className="container about__container">
+                <div className="about__me">
+                    <div className='about__me-image'>
+                        <img src={CLEMENT} alt="" />
+                    </div>
+                </div>
+                <div className="about__content">
+                    <article className="about__card">
+                        <FaAward className='about__icon' />
+                        <h5>expérience</h5>
+                        <small>A la recherche de ma premiere expérience </small>
+                    </article>
+
+                    <article className="about__card">
+                        <FaAward className='about__icon' />
+                        <h5>Formation</h5>
+                        <small>A la recherche de ma premiere expérience </small>
+                    </article>
+                </div>
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, ab.
+                </p>
+
+                <a href="#contact" className='btn btn--primary'>let's talk</a>
+
+
+            </div>
+        </section>
+    );
 }
 //about.propTypes = {};
 
