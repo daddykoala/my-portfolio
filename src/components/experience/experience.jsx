b7bd68c655b748086a29042b64de6d188bb8f6d5
daddykoala <moretticlement@outlook.fr>
2022-11-07 21:43:45 +0100

    feat&style: experience component desktop device

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -1,15 +1,91 @@
 import React from 'react';
-//import PropTypes from 'prop-types';
 
+import { BsPatchCheckFill } from 'react-icons/bs';
 import './experienceStyles.scss';
 
 function Experience() {
-return (
-    <section id="experience">experience</section>
-);
-}
-//experience.propTypes = {};
+    return (
+        <section id="experience">
+            <h5>What skills I have </h5>
+            <h2>My Experience</h2>
 
-//experience.defaultProps = {};
+            <div className="container container__experience">
+                <div className="experience__frontend">
+                    <h3>Frontend development</h3>
+                    <div className="experience__content">
+
+                        <article className="experience__details">
+                            <BsPatchCheckFill className='experience__details-icons' />
+                            <div>
+                                <h4>HTML</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+
+                        <article className="experience__details">
+                            <BsPatchCheckFill className='experience__details-icons' />
+                            <div>
+                                <h4>CSS</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+                        </article>
+
+                        <article className="experience__details">
+                            <BsPatchCheckFill className='experience__details-icons' />
+                            <div>
+                                <h4>JAVASCRIPT</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+
+                        </article>
+
+                        <article className="experience__details">
+                            <BsPatchCheckFill className='experience__details-icons' />
+                            <div>
+                                <h4>REACT</h4>
+                                <small className="text-light">Experienced</small>
+                            </div>
+
+                        </article>
+                    </div>
+
+                </div>
+                <div className="experience__backend">
+                    <h3>Backend development</h3>
+                    <div className="experience__content">
+
+                        <article className="experience__details">
+                            <BsPatchCheckFill className='experience__details-icons' />
+                            <div>
+                                <h4>NODE JS</h4>
+                                <small className="text-light">medium</small>
+                            </div>
+
+                        </article>
+
+                        <article className="experience__details">
+                            <BsPatchCheckFill className='experience__details-icons' />
+                            <div>
+
+                                <h4>EXPRESS JS</h4>
+                                <small className="text-light">medium</small>
+                            </div>
+                        </article>
+
+                        <article className="experience__details">
+                            <BsPatchCheckFill className='experience__details-icons' />
+                            <div>
+
+                            <h4>POSTGRES SQL</h4>
+                            <small className="text-light">medium</small>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
 
 export default React.memo(Experience);
