af81f82acbd27b9b0baf25e3f79f65e3a7fb3df3
daddykoala <moretticlement@outlook.fr>
2022-11-08 16:46:01 +0100

    add:style: css portfolio component desktop device

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -16,7 +16,7 @@ return (
         <a href="#" onClick={()=>setActiveNav('#')} className={activeNav==='#'?'active':''}><AiOutlineHome/></a>
         <a href="#about" onClick={()=>setActiveNav('#about')} className={activeNav==='#about'?'active':''}><BsFillPersonFill/></a>
         <a href="#experience" onClick={()=>setActiveNav('#experience')} className={activeNav==='#experience'?'active':''}><BsJournalBookmarkFill/></a>
-        <a href="#services" onClick={()=>setActiveNav('#services')} className={activeNav==='#services'?'active':''}><RiServiceLine/></a>
+        <a href="#portfolio" onClick={()=>setActiveNav('#services')} className={activeNav==='#services'?'active':''}><RiServiceLine/></a>
         <a href="#contact" onClick={()=>setActiveNav('#contact')} className={activeNav==='#contact'?'active':''}><IoMdMail/></a>
     </nav>
 );
--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -18,16 +18,24 @@ return (
                     <img src={DEVELOTT} alt="projet" />
                 </div>
                 <h3>Develott</h3>
+                <div className="portfolio__item-cta">
+
                 <a href="lien du projet" className='btn' target='_blank'>github</a>
                 <a href="site" className="btn btn-primary" target='_blank'>Live demo</a>
+                </div>
+                <p>Lorem ipsum dolor sistate, tempore minima odit rem doloremque la</p>
             </article>
             <article className="portfolio__item">
                 <div className="portfolio__item-image">
                     <img src={OKANBAN} alt="projet" />
                 </div>
                 <h3>O'kanban</h3>
+                <div className="portfolio__item-cta">
+
                 <a href="lien du projet" className='btn' target='_blank'>github</a>
                 <a href="site" className="btn btn-primary" target='_blank'>Live demo</a>
+                </div>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel, facilis laborum officiis voluptate, tempore minima odit rem doloremque la</p>
             </article>
         </div>
         
