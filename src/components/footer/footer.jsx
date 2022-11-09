321984cddd9d50e54edf7d53e3c9a4621e691c2a
daddykoala <moretticlement@outlook.fr>
2022-11-09 16:52:08 +0100

    add:footer component&style

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
-//import PropTypes from 'prop-types';
-
+import { AiFillGithub } from 'react-icons/ai';
+import { SiLinkedin } from 'react-icons/si';
 import './footerStyles.scss';
 
 function Footer() {
 return (
-    <div>
-        footer
+   <footer>
+    <a href="#" className='footer__head'>Clement Moretti</a>
+    <ul className='permalinks'>
+        <a href='#' >Home</a>
+        <a href='#About' >About</a>
+        <a href='#Experience' >Experience</a>
+        <a href='#portfolio' >Portfolio</a>
+        <a href='#contact' >Contact</a>
+    </ul>
+
+    <div className="footer__socials">
+        <a href=""><SiLinkedin/></a>
+        <a href=""><AiFillGithub/></a>
     </div>
+        
+    
+   </footer>
 );
 }
-//footer.propTypes = {};
 
-//footer.defaultProps = {};
 
 export default React.memo(Footer);
