4f0c96204b97758598d6eb9713c8ec4c599a2d1d
clément moretti <98534268+daddykoala@users.noreply.github.com>
2023-01-30 11:57:54 +0100

    fix: projet aude

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 //import PropTypes from 'prop-types';
 import DEVELOTT from '../../assets/DEVELOTT.png'
-import OKANBAN from '../../assets/OKANBAN.png'
+import AUDE from '../../assets/aude.jpg'
 
 import './portfolioStyles.scss';
 
@@ -20,14 +20,30 @@ return (
                 <h3>Develott</h3>
                 <div className="portfolio__item-cta">
 
-                <a href="https://github.com/daddykoala/my-portfolio" className='btn' target='_blank'>github</a>
-                <a href="https://develott-front.herokuapp.com/" className="btn btn-primary" target='_blank'>Live demo</a>
-                <a href="site" className="btn btn-primary" target='_blank'>Doc back</a>
+                <a href="https://github.com/daddykoala/develott-back" className='btn' target='_blank'>github</a>
+                <a href="https://develott.fr/" className="btn btn-primary" target='_blank'>Live demo</a>
+                <a href="https://api.develott.fr/api-docs/" className="btn btn-primary" target='_blank'>Doc back</a>
                 </div>
                 <p>Projet professionnel de fin d'année,Develott est une plateforme où les développeurs junior peuvent monter des projets avec d'autres développeurs junior.</p>
             </article>
+
+            <article className="portfolio__item">
+                <div className="portfolio__item-image">
+                    <img src={AUDE} alt="projet" />
+                </div>
+                <h3>Aude Renson</h3>
+                <div className="portfolio__item-cta">
+
+                <a href="https://github.com/daddykoala/Aude-renson" className='btn' target='_blank'>github</a>
+                <a href="https://xn--aude-renson-consultante-en-parentalit-ckd.com/" className="btn btn-primary" target='_blank'>Live demo</a>
+                
+                </div>
+                <p>Projet réalisé en react pour une connaissance qui commence son activité dans le conseil en parentalité</p>
+            </article>
+
+
             
-            <article className="portfolio__item none">
+            {/* <article className="portfolio__item none">
                 <div className="portfolio__item-image">
                     <img src={OKANBAN} alt="projet" />
                 </div>
@@ -41,7 +57,7 @@ return (
                 </div>
                 <p>Projet en cours de réalisation.
                     je souhaite simplement reproduire un trello en utilisant react, materialUI et mongoDB Vous pouvez venir voir mon avancement directement sur github.</p>
-            </article>
+            </article> */}
         </div>
         
     </section>
